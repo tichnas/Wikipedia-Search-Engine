3065ce54910eb24c92b3751f1091cf3a30f5a35d
tichnas <sanchit.arora.2002@gmail.com>
2021-08-28 02:46:45 +0530

    Separate Infobox & Category tokens from body tokens

--- indexer.py
+++ indexer.py
@@ -9,8 +9,10 @@ from constants import DATA_FILE, PAGES_IN_FILE
 ##   token -> [
 ##       [
 ##           page num,
-##           freq of token in title,
-##           freq of token in text
+##           freq of token in Title,
+##           freq of token in Body,
+##           freq of token in Infobox,
+##           freq of token in Category,
 ##       ],
 ##       ...
 ##   ]
@@ -59,13 +61,60 @@ class XMLHandler(xml.sax.ContentHandler):
 
 
 def tokenize(data):
-    return re.split(r"[^a-zA-Z0-9]+", data)
+    to_keep = ["Infobox", "}}", "[[Category:", "]]"]
+    escaped_to_keep = ["Infobox", "\}\}", "\[\[Category:", "\]\]"]
+
+    left = re.split("(" + ")|(".join(escaped_to_keep) + ")", data)
+    ans = []
+
+    for token in left:
+        if not token:
+            continue
+        if token in to_keep:
+            ans.append(token)
+        else:
+            ans.extend(re.split("[^a-zA-Z0-9]+", token))
+
+    return ans
 
 
 def index_page():
-    types = [title, text]
-    for i in range(0, len(types)):
-        index_tokens(i + 1, tokenize(types[i]))
+    index_tokens(1, tokenize(title))
+
+    text_tokens = tokenize(text)
+
+    body_tokens = []
+    infobox_tokens = []
+    category_tokens = []
+
+    is_special = False  # Anything True will result in special as True
+    is_infobox = False
+    is_category = False
+
+    for token in text_tokens:
+        if token == "Infobox" and not is_special:
+            is_infobox = True
+            is_special = True
+        elif token == "[[Category:" and not is_special:
+            is_category = True
+            is_special = True
+        elif token == "}}" and is_infobox:
+            is_infobox = False
+            is_special = False
+        elif token == "]]" and is_category:
+            is_category = False
+            is_special = False
+        elif token.isalnum():
+            if is_infobox:
+                infobox_tokens.append(token)
+            elif is_category:
+                category_tokens.append(token)
+            else:
+                body_tokens.append(token)
+
+    index_tokens(2, body_tokens)
+    index_tokens(3, infobox_tokens)
+    index_tokens(4, category_tokens)
 
 
 def index_tokens(type, tokens):
@@ -77,7 +126,7 @@ def index_tokens(type, tokens):
             index[token] = []
 
         if not index[token] or index[token][-1][0] != page_num:
-            index[token].append([page_num, 0, 0])
+            index[token].append([page_num, 0, 0, 0, 0])
 
         index[token][-1][type] += 1
 
